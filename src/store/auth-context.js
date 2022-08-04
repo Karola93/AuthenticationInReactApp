08cfaedf8f887080001b04048477c3de2c34fc4a
Karolina Dudek <karolinadudek9@gmail.com>
2022-08-04 09:48:43 +0200

    delate name error

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -47,9 +47,9 @@ export const AuthContextProvider = (props) => {
     }
 
     return (
-        <AuthContext.Procider value={contextValue}>
+        <AuthContext.Provider value={contextValue}>
             {props.children}
-        </AuthContext.Procider>
+        </AuthContext.Provider>
     )
 };
 
